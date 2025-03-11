b4d1b6fb61df38fa8c566f0f4e43c4d170c33af7
joanaquadzie <joanaq03@gmail.com>
2025-03-11 10:03:40 +0000

    editted the send nessage button

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -6,6 +6,7 @@ export default function Contact() {
   const [email, setEmail] = useState("");
   const [subject, setSubject] = useState("");
   const [message, setMessage] = useState("");
+
   return (
     <section id="contact">
       <div>
